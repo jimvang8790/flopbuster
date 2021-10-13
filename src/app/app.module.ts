d22413acc732c332b1b0b0fdc34f165b86208434
Jim <vangjim15@gmail.com>
2021-10-13 03:05:29 -0500

    create cart component with inline

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VoteComponent } from './vote/vote.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VoteComponent
+    VoteComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
