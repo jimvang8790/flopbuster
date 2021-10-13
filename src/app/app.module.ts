722237cbee1eef3ec724eccda9d3022212380c41
Jim <vangjim15@gmail.com>
2021-10-13 03:01:32 -0500

    created vote component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VoteComponent } from './vote/vote.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VoteComponent
   ],
   imports: [
     BrowserModule,
